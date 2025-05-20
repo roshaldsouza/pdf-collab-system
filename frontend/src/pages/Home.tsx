0572a294c698dc00d3c997da737e5e22787aa454
RoshalDsouza24 <roshalds789@gmail.com>
2025-05-20 11:42:32 +0530

    Setup frontend React app with basic routing and home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to PDF Management & Collaboration System</h1>
+    </div>
+  );
+};
+
+export default Home;
